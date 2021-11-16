1863744645234a4c13a330fe5830622a18c99b9d
Daniel Caetano de Souza Ferreira <danielcaetano.sf@gmail.com>
2021-11-16 12:55:28 -0300

    Adicionado tipos booleano e constante

--- jogoadivinhacao.cpp
+++ jogoadivinhacao.cpp
@@ -4,8 +4,10 @@ using namespace std;
 
 int main() {
   system("cls ");
-  int numeroSecreto = 42;
+  const int numeroSecreto = 42;
   int chute;
+  bool igual = numeroSecreto == chute;
+  bool maior = numeroSecreto >= chute;
 
   cout << "*********************************************" << endl;
   cout << "******BEM VINDOS AO JOGO DA ADIVINHAÇÃO******" << endl;
@@ -15,9 +17,9 @@ int main() {
     cout << "Qual seu chute?" << endl;
     cin >> chute;
 
-    if (chute == numeroSecreto) {
+    if (igual) {
       cout << "Parabéns, você acertou" << endl;
-    } else if (chute >= numeroSecreto) {
+    } else if (maior) {
       cout << "Seu chute é maior que o numero secreto!" << endl;
     } else {
       cout << "Seu chute é menor que o numero secreto!" << endl;
