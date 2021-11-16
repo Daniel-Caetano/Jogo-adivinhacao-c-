0ef5e39baaaaa63a7078033599431f7181d0d9b0
Daniel Caetano de Souza Ferreira <danielcaetano.sf@gmail.com>
2021-11-16 11:22:03 -0300

    Adicionado Loop e verificando se ganhou

--- jogoadivinhacao.cpp
+++ jogoadivinhacao.cpp
@@ -3,10 +3,25 @@
 using namespace std;
 
 int main() {
+  system("cls ");
   int numeroSecreto = 42;
+  int chute;
+
   cout << "*********************************************" << endl;
   cout << "******BEM VINDOS AO JOGO DA ADIVINHAÇÃO******" << endl;
   cout << "*********************************************" << endl;
+  do {
 
-  cout << "Numero secreto é:" << numeroSecreto << "Não conte a ninguém" << endl;
+    cout << "Qual seu chute?" << endl;
+    cin >> chute;
+
+    if (chute == numeroSecreto) {
+      cout << "Parabéns, você acertou" << endl;
+    } else if (chute >= numeroSecreto) {
+      cout << "Seu chute é maior que o numero secreto!" << endl;
+    } else {
+      cout << "Seu chute é menor que o numero secreto!" << endl;
+    }
+
+  } while (1);
 }
