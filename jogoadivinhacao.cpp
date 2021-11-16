2c58f9e86ad4a2c2224e69be793132c7abc4f5a1
unknown <danielcaetano.sf@gmail.com>
2021-11-16 10:44:53 -0300

    Abertura e numero secreto implementado

---
+++ jogoadivinhacao.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int numeroSecreto = 42;
+  cout << "*********************************************" << endl;
+  cout << "******BEM VINDOS AO JOGO DA ADIVINHAÇÃO******" << endl;
+  cout << "*********************************************" << endl;
+
+  cout << "Numero secreto é:" << numeroSecreto << "Não conte a ninguém" << endl;
+}
