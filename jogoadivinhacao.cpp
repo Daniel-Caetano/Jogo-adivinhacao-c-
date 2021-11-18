275e830a828b85e462b70cafdd4751caa25a98fb
Daniel Caetano de Souza Ferreira <danielcaetano.sf@gmail.com>
2021-11-18 08:54:51 -0300

    Jogo adivinhação finalizado

--- jogoadivinhacao.cpp
+++ jogoadivinhacao.cpp
@@ -1,3 +1,4 @@
+#include <Windows.h>
 #include <iostream>
 
 using namespace std;
@@ -9,6 +10,37 @@ void abertura() {
   cout << "*********************************************" << endl;
 }
 
+int dificuldade() {
+  int nivel;
+  int nTentativas;
+  cout << "\nEscolha o nivel do desafio:" << endl;
+  cout << "__________________" << endl;
+  cout << "|[1] = Facil     |" << endl;
+  cout << "|[2] = Medio     |" << endl;
+  cout << "|[3] = Dificil   |" << endl;
+  cout << "------------------" << endl;
+  cin >> nivel;
+
+  switch (nivel) {
+  case 1:
+    nTentativas = 20;
+    break;
+  case 2:
+    nTentativas = 13;
+    break;
+  case 3:
+    nTentativas = 5;
+    break;
+  default:
+    system("cls || clear");
+    cout << "COMANDO INVALIDO" << endl;
+    Sleep(1500);
+    dificuldade();
+  }
+
+  return nTentativas;
+}
+
 int main() {
   const int NUMERO_SECRETO = 42;
 
@@ -17,11 +49,12 @@ int main() {
   bool maior;
 
   int chute;
-  int tentativas = 0;
+  int tentativas = 1;
 
   double pontuacao = 1000.00;
 
   abertura();
+  const int _NIVEL = dificuldade();
 
   do {
 
@@ -32,29 +65,31 @@ int main() {
 
     if (igual) {
       system("cls");
-      cout << "\nParabéns, você acertou em " << cout.precision(2) << tentativas
-           << " tentativas" << endl;
+      cout << "\nParabéns, você acertou em " << tentativas << " tentativas"
+           << endl;
       cout.precision(2);
       cout << fixed;
       cout << "Sua pontuação foi de :|" << pontuacao << "|" << endl;
-
       naoAcertou = false;
+      break;
     } else if (maior) {
       cout << "Seu chute é maior que o numero secreto!" << endl;
       double pontosPerdidos = abs(chute - NUMERO_SECRETO) / 2.0;
       pontuacao = pontuacao - pontosPerdidos;
-      tentativas++;
     } else {
       cout << "Seu chute é menor que o numero secreto!" << endl;
       double pontosPerdidos = abs(chute - NUMERO_SECRETO) / 2.0;
       pontuacao = pontuacao - pontosPerdidos;
-      tentativas++;
     }
 
-    // if (tentativas - 1 == 5) {
-    // system("cls");
-    // cout << "Você perdeu!!" << endl;
-    // naoAcertou = false;
-    // }
+    if (tentativas == _NIVEL) {
+      system("cls || clear");
+      naoAcertou = false;
+      cout << "Você perdeu!!" << endl;
+      cout << "Numero de tentativas: " << tentativas << endl;
+      Sleep(1000);
+      break;
+    }
+    tentativas++;
   } while (naoAcertou);
 }
